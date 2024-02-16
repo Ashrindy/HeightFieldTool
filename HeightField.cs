760aa2906945013406668be61d4c3d7c2026e56a
Ashrindy <honzula1000@gmail.com>
2024-02-16 19:33:23 +0100

    reading

---
+++ HeightField.cs
@@ -0,0 +1,39 @@
+﻿using Amicitia.IO.Binary;
+using System.Numerics;
+
+namespace HeightFieldTool;
+
+public class HeightField
+{
+    public string Signature { get; set; }
+    public int Field08 { get; set; }
+    public int Field0c { get; set; }
+    public int Field10 { get; set; }
+    public Vector2 ImageSize { get; set; }
+    public float Field1c { get; set; }
+    public float Field20 { get; set; }
+    public float Field24 { get; set; }
+    public float Field28 { get; set; }
+    public UInt16[] Pixels { get; set; }
+    public Int32 Field30 { get; set; }
+    public Vector3[] Unk1 { get; set; }
+    public byte[] Data { get; set; }
+
+    public void Read(BinaryObjectReader reader)
+    {
+        Signature = reader.ReadString(StringBinaryFormat.FixedLength, 4);
+        Field08 = reader.Read<int>();
+        Field0c = reader.Read<int>();
+        Field10 = reader.Read<int>();
+        ImageSize = reader.Read<Vector2>();
+        Field1c = reader.Read<float>();
+        Field20 = reader.Read<float>();
+        Field24 = reader.Read<float>();
+        Field28 = reader.Read<float>();
+        Pixels = reader.ReadArray<UInt16>(16777216);
+        Field30 = reader.Read<int>();
+        Unk1 = reader.ReadArray<Vector3>(11);
+        Data = reader.ReadArray<byte>(16769024);
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amicitia.IO.Binary;
+using System.Text;
+using System.Drawing;
+
+namespace HeightFieldTool;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string filename;
+
+        if(args.Length > 0) 
+        {
+            filename = args[0];
+        }
+        else
+        {
+            Console.WriteLine("What's the file?");
+            filename = Console.ReadLine();
+        }
+
+        BinaryObjectReader reader = new(filename, Endianness.Little, Encoding.UTF8);
+
+        if(filename.EndsWith(".heightfield"))
+        {
+            HeightField hf = new();
+            hf.Read(reader);
+
+            Bitmap image = new(width: (int)hf.ImageSize.X, height: (int)hf.ImageSize.Y);
+
+            Graphics graphics = Graphics.FromImage(image);
+
+            int index = 0;
+
+            foreach (uint i in hf.Pixels)
+            {
+                Pen pen = new Pen(Color.FromArgb(255, checked((int)i), checked((int)i), checked((int)i)));
+                Rectangle rectangle = new Rectangle(index - 4096 * (index / 4096), (index / 4096), 1, 1);
+                graphics.DrawRectangle(pen, rectangle);
+                index++;
+            }
+
+            string filePath = filename + ".jpg";
+            image.Save(filePath);
+
+            graphics.Dispose();
+            image.Dispose();
+        }
+    }
+}
